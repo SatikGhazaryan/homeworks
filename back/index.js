8c3f3094b93c60bbb1840c9e5cae124a964d25dd
Satik Ghazryan <satik@Satiks-MacBook-Air.local>
2022-06-27 22:53:54 +0400

    json middleware use

--- back/index.js
+++ back/index.js
@@ -1,7 +1,10 @@
 let express = require("express");
 const mongoose = require('mongoose')
 const app =express(); 
+
 const port = 8000;
+const json =express.json();
+app.use(json)
 app.listen(port, ()=> console.log(`server is listening port ${port}`));
 (async () => {
   await mongoose.connect('mongodb://localhost:27017/myProject')
